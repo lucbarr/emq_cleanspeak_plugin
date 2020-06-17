a5c0b4f0920c1510c784e980ef2e9e8bffac075d
Luciano Barreira <luciano.barreira@wildlifestudios.com>
2020-06-17 16:56:28 -0300

    Change app name

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule EmqThrottlePlugin.Mixfile do
 
   def project do
     [
-      app: :emq_throttle_plugin,
+      app: :emq_cleanspeak_plugin,
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
