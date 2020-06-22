6236f8f8eed76708d67e46b711d90c547b01ad37
Luciano Barreira <luciano.barreira@wildlifestudios.com>
2020-06-22 16:21:24 -0300

    Remove throttle plugin

--- config/config.exs
+++ config/config.exs
@@ -10,11 +10,11 @@ use Mix.Config
 
 # You can configure your application as:
 #
-#     config :emq_throttle_plugin, key: :value
+#     config :emq_cleanspeak_plugin, key: :value
 #
 # and access this configuration in your application as:
 #
-#     Application.get_env(:emq_throttle_plugin, :key)
+#     Application.get_env(:emq_cleanspeak_plugin, :key)
 #
 # You can also configure a 3rd-party app:
 #
